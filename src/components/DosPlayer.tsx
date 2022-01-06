20c915b7fb8405410f2fd4e47d89fcd688b1aee3
Logan Scheid <scheid.logan@gmail.com>
2022-01-05 20:49:42 -0500

    jsdos functionality added, started converting css into modules

---
+++ src/components/DosPlayer.tsx
@@ -0,0 +1,42 @@
+import React, { useRef, useEffect } from "react";
+import DosboxCSS from "../style/Dosbox.module.css";
+import { DosFactory } from "js-dos";
+
+require("js-dos");
+
+const Dos = (window as any).Dos as DosFactory;
+let doomUrl = "https://loganscheid.dev/assets/games/DOOM.zip"
+
+const DosPlayer = ({game, startGame, exitGame}) => {
+    let gameName = game.slice(game.lastIndexOf('/') + 1, game.lastIndexOf('.')) + ".EXE"    
+    const ref = useRef<HTMLCanvasElement>(null);
+
+
+    useEffect(() => {
+        if (ref !== null && game !== null) {
+            const ciPromise = Dos(ref.current as HTMLCanvasElement, {
+                wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js",
+                cycles: "auto",
+                autolock: false,
+            }).then((runtime) => {
+                return runtime.fs.extract(game).then(() => {
+                    return runtime.main(["-c", gameName]);
+                });
+            });
+
+            return () => {
+                ciPromise.then(ci => ci.exit());
+            };
+        }
+        console.log('here')
+    }, [ref]);
+
+    return (
+        <div id="game-container">
+            <canvas style={{width: '640px', height: "480px"}} ref={ref} />
+            <button onClick={exitGame}>Eject</button>
+        </div>
+    )
+}
+
+export default DosPlayer;
--- src/components/Dosbox.js
+++ src/components/Dosbox.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react'
+import DosPlayer from './DosPlayer.tsx';
+import DosboxCSS from "../style/Dosbox.module.css";
+
+const DOOM_URL = "https://loganscheid.dev/assets/games/DOOM.zip"
+const SABA_URL = "https://loganscheid.dev/assets/games/SABA.zip"
+const MSPAC_URL = "https://loganscheid.dev/assets/games/MSPAC.zip"
+const CHESS_URL = "https://loganscheid.dev/assets/games/CHESS.zip"
 
 const Dosbox = () => {
+    const [game, setGame] = useState(null);
+
+    const exitGame = () =>{
+        setGame(null)
+    }
+
+    
+    const startGame = (gameName) =>{
+        setGame(gameName)
+    }
+
+    /**Add and remove dosplayer based on clicked game buttons, doom adds doom dosplayer.tsx, exit deletes dosplayer element */
     return (
-        <div id="dosbox" style={{border: 'groove'}}>
-            <p>Dosbox</p>
+        <div id={DosboxCSS.dosbox}>
+            {game ? (
+                <DosPlayer game={game} exitGame={exitGame} startGame={startGame}/>
+            )
+            : (
+                <>
+                    <div id={DosboxCSS['game-screen']}>
+                        <p>pick a game below</p>
+                    </div>
+                    <div id={DosboxCSS.controls}>
+                        <button onClick={() => setGame(DOOM_URL)}>Doom</button>
+                        <button onClick={() => setGame(SABA_URL)}>Acid Tetris</button>
+                        <button onClick={() => setGame(MSPAC_URL)}>Ms.Pac-Man</button>
+                        <button onClick={() => setGame(CHESS_URL)}>Chess88</button>
+                    </div>
+                </>
+            )
+            } 
         </div>
     )
 }
