3905db433267ecefd42acabac30e6f41f25beee0
Logan Scheid <scheid.logan@gmail.com>
2022-05-06 03:37:55 -0400

    more small style improvements to dashboard

--- src/components/FeedEntry.jsx
+++ src/components/FeedEntry.jsx
@@ -5,12 +5,12 @@ import {FaTrashAlt, FaExternalLinkAlt} from "react-icons/fa";
 const FeedEntry = () => {
     return (
         <tr className={FeedCSS.feedRow}>
-            <td>Bob (0x090132...)</td>
-            <td>Sally (0xas42t2...)</td>
+            <td><a href="#">0x82sa...4d2z <FaExternalLinkAlt/></a></td>
+            <td><a href="#">0x82sa...4d2z <FaExternalLinkAlt/></a></td>
             <td>Atomic Match_</td>
             <td>40 Ξ</td>
             <td>2021-12-30 04:45</td>
-            <td><a href="#">0x82sas7... <FaExternalLinkAlt/></a></td>
+            <td><a href="#">0x82sa...4d2z <FaExternalLinkAlt/></a></td>
             <td><FaTrashAlt/></td>
         </tr>
     )
