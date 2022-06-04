e16cc3e4be510f3f68d9595b68033fecd19cf85d
Logan Scheid <scheid.logan@gmail.com>
2022-06-03 22:07:00 -0400

    added layout to local storage for persistence

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -11,26 +11,44 @@ import "../../node_modules/react-grid-layout/css/styles.css";
 import "../../node_modules/react-resizable/css/styles.css";
 
 const ReactGridLayout = WidthProvider(RGL);
+const originalLayout = getFromLS("layout") || [];
+
+function getFromLS(key) {
+  let ls = {};
+  if (window.localStorage) {
+    try {
+      ls = JSON.parse(window.localStorage.getItem("rgl-7")) || {};
+    } catch (e) {
+      /*Ignore*/
+    }
+  }
+  console.log("get from LS", ls[key]);
+  return ls[key];
+}
+
+function saveToLS(key, value) {
+  if (window.localStorage) {
+    window.localStorage.setItem(
+      "rgl-7",
+      JSON.stringify({
+        [key]: value,
+      })
+    );
+  }
+}
 
 const Dashboard = ({ account, handleAccount }) => {
   const [addresses, setAddresses] = useState([]);
   const [block, setBlock] = useState(null);
-  const layout = [
-    { i: "Feed", x: 0, y: 0, w: 12, h: 68, minW: 8, minH: 7 },
-    { i: "Watchlist", x: 12, y: 0, w: 4, h: 34, minW: 3, minH: 7, maxH: 68 },
-    {
-      i: "Trollbox",
-      x: 16,
-      y: 0,
-      w: 4,
-      h: 34,
-      maxW: 4,
-      minW: 3,
-      minH: 7,
-      maxH: 68,
-    },
-    { i: "Dosbox", x: 12, y: 68, w: 8, h: 34, minW: 5, minH: 20 },
-  ];
+  const [layout, setLayout] = useState(
+    JSON.parse(JSON.stringify(originalLayout))
+  );
+
+  const onLayoutChange = (layout) => {
+    /*eslint no-console: 0*/
+    saveToLS("layout", layout);
+    setLayout(layout);
+  };
 
   return (
     <main style={{ alignItems: "unset" }}>
@@ -47,21 +65,43 @@ const Dashboard = ({ account, handleAccount }) => {
         rowHeight={10}
         allowOverlap={false}
         margin={[5, 5]}
+        onLayoutChange={onLayoutChange}
       >
-        <div key="Feed">
+        <div
+          key="Feed"
+          data-grid={{ x: 0, y: 0, w: 12, h: 68, minW: 8, minH: 7 }}
+        >
           <Feed block={block} account={account} addresses={addresses} />
         </div>
-        <div key="Watchlist">
+        <div
+          key="Watchlist"
+          data-grid={{ x: 12, y: 0, w: 4, h: 34, minW: 3, minH: 7, maxH: 68 }}
+        >
           <WatchList
             account={account}
             addresses={addresses}
             setAddresses={setAddresses}
           />
         </div>
-        <div key="Trollbox">
+        <div
+          key="Trollbox"
+          data-grid={{
+            x: 16,
+            y: 0,
+            w: 4,
+            h: 34,
+            maxW: 5,
+            minW: 3,
+            minH: 7,
+            maxH: 68,
+          }}
+        >
           <Trollbox account={account} />
         </div>
-        <div key="Dosbox">
+        <div
+          key="Dosbox"
+          data-grid={{ x: 12, y: 68, w: 8, h: 34, minW: 5, minH: 20 }}
+        >
           <Dosbox />
         </div>
       </ReactGridLayout>
